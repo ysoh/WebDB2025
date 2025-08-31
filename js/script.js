a6993807aaa3d74b22aa5f27ef7563b903f03fff
Yunsang Oh <ysoh0908@gmail.com>
2025-08-31 13:56:47 +0900

    2025-08-31 13:56

--- js/script.js
+++ js/script.js
@@ -25,13 +25,21 @@ document.addEventListener('DOMContentLoaded', function () {
     const songImageInput = document.getElementById('song_image');
     const fileNameDisplay = document.getElementById('file-name');
 
-    songImageInput.addEventListener('change', function () {
-        if (this.files.length > 0) {
-            fileNameDisplay.textContent = this.files[0].name;
-            fileNameDisplay.style.color = '#333';
-        } else {
-            fileNameDisplay.textContent = '선택된 파일 없음';
-            fileNameDisplay.style.color = '#888';
-        }
-    });
+    if (songImageInput && fileNameDisplay) { // 요소가 존재하는지 확인
+        songImageInput.addEventListener('change', function () {
+            if (this.files.length > 0) {
+                fileNameDisplay.textContent = this.files[0].name;
+                fileNameDisplay.style.color = '#333';
+            } else {
+                fileNameDisplay.textContent = '선택된 파일 없음';
+                fileNameDisplay.style.color = '#888';
+            }
+        });
+    }
 });
+
+// 햄버거 메뉴 토글 함수
+function toggleMenu() {
+    const navLinksWrapper = document.querySelector('.nav-links-wrapper');
+    navLinksWrapper.classList.toggle('active');
+}
