3cbb70122abf09c4ba154891ed041f0bd3d21055
Yunsang Oh <ysoh0908@gmail.com>
2025-09-11 09:21:29 +0900

    2025-09-11 09:21

--- conf/db_config.php
+++ conf/db_config.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "infodb.ansan.ac.kr";
 $username = "i2551500"; // 실제 환경에 맞게 변경
-$password = "wjdqh..rhk"; // 실제 환경에 맞게 변경
-$dbname = "db2551500_webdb2025"; // 실제 환경에 맞게 변경
+$password = ""; // 실제 환경에 맞게 변경
+$dbname = "db2551500"; // 실제 환경에 맞게 변경
 $charset = "utf8mb4";
 
 // PDO DSN (Data Source Name)
